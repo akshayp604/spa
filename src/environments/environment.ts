5cb955594b399fc5becfc08e8e38fc3c4d19801d
trilok0604 <trilok.rawat@techinfini.com>
2022-01-18 11:40:34 +0530

    worked on login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,3 +15,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+//  ng build --prod --base-href https://cpx.covetus.com/spa1/
